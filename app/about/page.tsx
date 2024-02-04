5bf688a447141cb659f7ca0f1dc78290ebe25978
1200942MarcoFerraz <1200942@isep.ipp.pt>
2024-02-04 20:18:35 +0000

    #comments Small fixes and adding the about page main content. To add: icons to important stuff

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,51 @@
+"use client"
+
 import './page.css'
+import React, { useState, useEffect } from 'react';
 
 const page = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 300);
+    };
+
+    if (typeof window !== 'undefined') {
+      setIsSmallScreen(window.innerWidth <= 300);
+
+      window.addEventListener('resize', handleResize);
+    }
+
+    return () => {
+      // Remove event listener on component unmount
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleResize);
+      }
+    };
+  }, []);
+
+  const handleTownClick = (event: any) => {
+    if (event?.target.innerText == "Marco de Canaveses") {
+      window.open("https://www.google.com/search?q=marco%20de%20canaveses");
+    }
+  }
+
   return (
     <div className="about-me-page">
       <span className='page-header'>
-        Marco Ferraz
+        {isSmallScreen ? "Marco Fz." : "Marco Ferraz"}
       </span>
+        <div className='page-presentation'>        
+          <span className='presentation-message' onClick={handleTownClick}>
+            <strong>Hello</strong> Reader! <br></br> Born in 2002, in a small portuguese town,&nbsp;
+            <u>Marco de Canaveses</u>,
+            I'm a software developer who loves learning new technologies and areas of the IT&nbsp; 
+            <strong>world</strong>.<br></br>
+            From the freedom that comes with personal web development, a newfound passion emerged:
+            learning how to build simple, intuitive and harmonious web applications.
+          </span>      
+        </div>
     </div>
   );
 };
