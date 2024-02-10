20a879d6ec93295553f638f7c2a602205f985011
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-02-10 18:50:08 +0000

    #comments Adding new functionalities to the navigation, click outside sidebar now closes it.

--- app/components/sidenavbar/sidenavbar.tsx
+++ app/components/sidenavbar/sidenavbar.tsx
@@ -2,7 +2,7 @@
 
 import './sidenavbar.css'
 import Link from 'next/link';
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { TbUserStar, TbUserCode, TbUserQuestion } from 'react-icons/tb'
 import { PiChatCircleDotsBold } from 'react-icons/pi'
@@ -35,17 +35,36 @@ const menuSecondaryItems=[
 
 interface SideNavBarProps {
   onItemClick: (itemName: string) => void;
+  onLayoutNotification: boolean;
+  onSidebarNotification: () => void;
 }
 
-export default function SideNavBar({ onItemClick }: SideNavBarProps) {
+export default function SideNavBar({ onItemClick, onLayoutNotification, onSidebarNotification }: SideNavBarProps) {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [clickedMainIndex, setClickedMainIndex] = useState(-1);
+    const [clickedSecondaryIndex, setClickedSecondaryIndex] = useState(-1);
+    
+    useEffect(() => {
+      if (onLayoutNotification) {
+        setIsSidebarOpen(false);
+      }
+    }, [onLayoutNotification]);
 
-    const handleItemClick = (itemName: string) => {
+    const handleMainItemClick = (itemName: string, index: number) => {
       onItemClick(itemName);
+      setClickedMainIndex(index);
+      setClickedSecondaryIndex(-1);
+    };
+
+    const handleSecondaryItemClick = (itemName: string, index: number) => {
+      onItemClick(itemName);
+      setClickedSecondaryIndex(index);
+      setClickedMainIndex(-1);
     };
 
     const toggleSidebar = () => {
       setIsSidebarOpen(!isSidebarOpen);
+      onSidebarNotification();
     };
 
     return (
@@ -63,9 +82,13 @@ export default function SideNavBar({ onItemClick }: SideNavBarProps) {
               <div className='options'>                     
                 {
                     menuMainItems.map((item, index) => (
-                        <Link href={item.path} key={index} className="option group" onClick={() => handleItemClick(item.name)}>
-                            <div className="group-hover:text-white icon">{item.icon}</div>                    
-                            <h3 className="group-hover:text-white link-text">{item.name}</h3>
+                        // <Link href={item.path} key={index} className="option group" onClick={() => handleItemClick(item.name)}>
+                        //     <div className="group-hover:text-white icon">{item.icon}</div>                    
+                        //     <h3 className="group-hover:text-white link-text">{item.name}</h3>
+                        // </Link>
+                        <Link href={item.path} key={index} className={`option group ${clickedMainIndex === index ? 'clicked-option' : ''}`} onClick={() => handleMainItemClick(item.name, index)}>
+                          <div className={`group-hover:text-white icon ${clickedMainIndex === index ? 'clicked-icon' : ''}`}>{item.icon}</div>                    
+                          <h3 className={`group-hover:text-white link-text ${clickedMainIndex === index ? 'clicked-text' : ''}`}>{item.name}</h3>
                         </Link>
                     ))
                 }                      
@@ -73,9 +96,13 @@ export default function SideNavBar({ onItemClick }: SideNavBarProps) {
               <div className='options'>                     
                 {
                     menuSecondaryItems.map((item, index) => (
-                        <Link href={item.path} key={index} className="option group" onClick={() => handleItemClick(item.name)}>
-                            <div className="group-hover:text-white icon" style={{fontSize:'1.5rem'}}>{item.icon}</div>                    
-                            <h3 className="group-hover:text-white link-text" style={{paddingRight:'10px'}}>{item.name}</h3>
+                        //  <Link href={item.path} key={index} className="option group" onClick={() => handleItemClick(item.name)}>
+                        //      <div className="group-hover:text-white icon" style={{fontSize:'1.5rem'}}>{item.icon}</div>                    
+                        //      <h3 className="group-hover:text-white link-text" style={{paddingRight:'10px'}}>{item.name}</h3>
+                        //  </Link>
+                        <Link href={item.path} key={index} className={`option group ${clickedSecondaryIndex === index ? 'clicked-option' : ''}`} onClick={() => handleSecondaryItemClick(item.name,index)}>
+                            <div className={`group-hover:text-white icon ${clickedSecondaryIndex === index ? 'clicked-icon' : ''}`}>{item.icon}</div>                    
+                            <h3 className={`group-hover:text-white link-text ${clickedSecondaryIndex === index ? 'clicked-text' : ''}`} style={{paddingRight: '20px'}}>{item.name}</h3>
                         </Link>
                     ))
                 }                      
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,20 +20,54 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const [itemName, setItemName] = useState("Home");
+  const [sidebarNotification, setSidebarNotification] = useState(false);
 
   const handleItemClick = (itemName: string) => {
     setItemName(itemName);
   };
 
+  const handleHtmlClick = (event: any) => {
+    const clickedElement = event.target as HTMLElement;
+  
+    // Function to check if the element belongs to the sidebar
+    const hasClassOrAncestor = (element: HTMLElement, className: string): boolean => {
+      if (element.classList.contains(className)) {
+        return true;
+      } else if (element.parentElement) {
+        return hasClassOrAncestor(element.parentElement, className);
+      }
+      return false;
+    };
+  
+    // Call function 
+    const isSidebarClick = hasClassOrAncestor(clickedElement, 'sidebar');
+  
+    if (!sidebarNotification) {
+      if (isSidebarClick) {
+        console.log('Clicked on the sidebar or its buttons.');
+      } else {
+        console.log('Clicked on other element.');
+        setSidebarNotification(true);
+      }
+    }
+
+    console.log(sidebarNotification);
+  };
+
+  // Reset to allow the sidebar to close again if opened and the user clicks outside the sidebar, and so on
+  const handleHtmlClickReset = () => {
+    setSidebarNotification(false);
+  };
+
   return (
-    <html lang="en">
+    <html lang="en" onClick={sidebarNotification ? undefined : handleHtmlClick}>
       <body className={inter.className}>   
         <div className='main-screen'>
           
           <div id="main-screen-addorns" className="main-screen-page">
             <div id="main-content">
               {/* <span id="navigation-marker">{itemName}</span> */} 
-              <SideNavBar onItemClick={handleItemClick}/>
+              <SideNavBar onItemClick={handleItemClick} onLayoutNotification={sidebarNotification} onSidebarNotification={handleHtmlClickReset}/>
               {children}
             </div>
             <div id="top-left-container">
