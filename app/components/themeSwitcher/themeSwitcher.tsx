9eb46290a5cc911d1d978c1f961c6a611d8a9ee5
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-02-13 22:26:10 +0000

    #comments Changing the themeSwitcher component location.

---
+++ app/components/themeSwitcher/themeSwitcher.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useTheme } from 'next-themes'
+import { useState, useEffect } from 'react'
+import './themeSwitcher.css'
+import { GoMoon, GoSun } from "react-icons/go";
+
+export default function ThemeSwitcher() {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+    const [isToggled, setIsToggled] = useState(theme == 'dark');
+
+    /*useEffect(() => {
+        setMounted(true);
+      }, []);
+
+    if(!mounted) {
+        return null;
+    }*/
+
+    const handleToggle = () => {
+        setIsToggled(!isToggled);
+
+        if(!isToggled) {
+            setTheme("dark");
+        } else {
+            setTheme("light");
+        }
+    }
+
+    return (
+        <div>
+            <div className="toggle">
+                <input title="Light/dark mode" type="checkbox" id="switch" onChange={handleToggle}/>
+                <label htmlFor="switch">
+                {isToggled ? (
+                    <GoMoon style={{ marginLeft: 'auto', color: 'rgb(209,177,164)'}}/>
+                    ) : (
+                    <GoSun style={{ color: 'rgb(52,60,96)' }}/>
+                    )}       
+                </label>
+            </div>
+        </div>    
+    )
+}
