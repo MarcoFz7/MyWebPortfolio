c6249c6a8dde789807db1eb59cb91183104670ee
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-03-02 17:03:13 +0000

    #comments using react hook useeffect select the navigation element corresponding to the item the user is viewing

--- app/components/sidenavbar/sidenavbar.tsx
+++ app/components/sidenavbar/sidenavbar.tsx
@@ -2,6 +2,7 @@
 
 import './sidenavbar.css'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { TbUserStar, TbUserCode, TbUserQuestion } from 'react-icons/tb'
@@ -44,6 +45,32 @@ export default function SideNavBar({ onItemClick, onLayoutNotification, onSideba
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     const [clickedMainIndex, setClickedMainIndex] = useState(-1);
     const [clickedSecondaryIndex, setClickedSecondaryIndex] = useState(-1);
+
+    const currentPage = usePathname();
+
+    // Used to set the corresponding navigation item as selected on refresh/start
+    useEffect(() => {
+      if (clickedMainIndex == -1) {
+        switch (currentPage) {
+          case "/about":
+            setClickedMainIndex(0);
+            break;
+          case "/experience":
+            setClickedMainIndex(1);
+            break;
+          case "/projects":
+            setClickedMainIndex(2);
+            break;
+        } 
+      }
+      if (clickedSecondaryIndex == -1) {
+        switch (currentPage) {
+          case "/contacts":
+            setClickedSecondaryIndex(0);
+            break;
+        } 
+      }
+    }, []);
     
     useEffect(() => {
       if (onLayoutNotification) {
@@ -82,11 +109,7 @@ export default function SideNavBar({ onItemClick, onLayoutNotification, onSideba
               </div>
               <div className='options'>                     
                 {
-                    menuMainItems.map((item, index) => (
-                        // <Link href={item.path} key={index} className="option group" onClick={() => handleItemClick(item.name)}>
-                        //     <div className="group-hover:text-white icon">{item.icon}</div>                    
-                        //     <h3 className="group-hover:text-white link-text">{item.name}</h3>
-                        // </Link>
+                    menuMainItems.map((item, index) => (                       
                         <Link href={item.path} key={index} className={`option group ${clickedMainIndex === index ? 'clicked-option' : ''}`} onClick={() => handleMainItemClick(item.name, index)}>
                           <div className={`group-hover:text-white icon ${clickedMainIndex === index ? 'clicked-icon' : ''}`}>{item.icon}</div>                    
                           <h3 className={`group-hover:text-white link-text ${clickedMainIndex === index ? 'clicked-text' : ''}`}>{item.name}</h3>
@@ -96,11 +119,7 @@ export default function SideNavBar({ onItemClick, onLayoutNotification, onSideba
               </div>
               <div className='options'>                     
                 {
-                    menuSecondaryItems.map((item, index) => (
-                        //  <Link href={item.path} key={index} className="option group" onClick={() => handleItemClick(item.name)}>
-                        //      <div className="group-hover:text-white icon" style={{fontSize:'1.5rem'}}>{item.icon}</div>                    
-                        //      <h3 className="group-hover:text-white link-text" style={{paddingRight:'10px'}}>{item.name}</h3>
-                        //  </Link>
+                    menuSecondaryItems.map((item, index) => (                       
                         <Link href={item.path} key={index} className={`option group ${clickedSecondaryIndex === index ? 'clicked-option' : ''}`} onClick={() => handleSecondaryItemClick(item.name,index)}>
                             <div className={`group-hover:text-white icon ${clickedSecondaryIndex === index ? 'clicked-icon' : ''}`}>{item.icon}</div>                    
                             <h3 className={`group-hover:text-white link-text ${clickedSecondaryIndex === index ? 'clicked-text' : ''}`} style={{paddingRight: '20px'}}>{item.name}</h3>
