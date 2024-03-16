6514300bf90a4e80bc406494d342ca5bfba6e60f
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-03-16 18:15:39 +0000

    #comments Adding some functionalities and improving the contact form connection with the contacts page itself

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,20 +1,30 @@
 "use client"
 
 import './page.css'
-import React, { } from 'react';
+import React, { useState } from 'react';
 import { FaLinkedinIn } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
 import { BiLogoGmail } from "react-icons/bi";
 import ContactForm from '../components/contactform/contactform';
 
 const Page = () => {
+  const [emailBtnClicked, setEmailBtnClicked] = useState(false);
+
   const handleLinkedInBtn = () => {
     window.open("https://pt.linkedin.com/in/marco-ferraz-03042025a");
   }
 
-  const handleGithubBtn = () => {
-    window.open("https://github.com/MarcoFz7");
+  const handleInstagramBtn = () => {
+    window.open("https://www.instagram.com/marcofz7");
   }
+
+  const handleEmailBtn = () => {
+    setEmailBtnClicked(true);
+  }
+
+  const resetEmailBtnClicked = () => {
+    setEmailBtnClicked(false);
+  };
 
   return (
     <div className="contacts-page">
@@ -25,15 +35,16 @@ const Page = () => {
         <button type='button' title='LinkedIn' className='linkedIn-btn' onClick={handleLinkedInBtn}>
             <FaLinkedinIn/>
         </button>
-        <button type='button' title='GitHub' className='gitHub-btn' onClick={handleGithubBtn}>
-            <FaGithub/>
+        <button type='button' title='Instagram' className='instagram-btn' onClick={handleInstagramBtn}>
+            <GrInstagram/>
         </button>
-        <button type='button' title='Email' className='email-btn'>
+        <button type='button' title='Email' className='email-btn' onClick={handleEmailBtn}>
             <BiLogoGmail/>
         </button>
       </div>
-      <div className='page-presentation'>        
-        <ContactForm/>              
+      <div className='page-presentation'>  
+        <label className='intro-message'>Reach out to me using <strong>social media</strong>, or...</label>
+        <ContactForm emailBtnClicked={emailBtnClicked} onEmailBtnProcessed={resetEmailBtnClicked}/>              
       </div>
     </div>
   );
