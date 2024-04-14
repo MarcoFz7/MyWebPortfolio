11ab18d69da9ed89fa663a1e08abd7f8d4c2464c
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-04-14 21:01:48 +0100

    #comments developing the experiences page (mainly aspect related stuff)

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,21 +1,15 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { FaLinkedinIn } from "react-icons/fa6";
 import './page.css';
 
+// progression property options: "New role!"; "New company!";"New company & role!";"" (empty for first item)
+/* 
+  progression was implemented as an extra propery since checking the company and role of 
+  consecutives items is heavier, mainly if one contains many experiences.
+*/
 const experiences=[
-  {
-    company: "test 1",
-    role: "test 1",
-    stack: [
-      "Angular.js ",
-      ".Net C# ", 
-      "JavaScript ", 
-      "D365 CRM "
-    ],
-    client: "test 1",
-    date: "test 1"
-  },
   {
       company: "n4IT",
       role: "D365 CRM Developer",
@@ -23,10 +17,12 @@ const experiences=[
         "Angular.js ",
         ".Net C# ", 
         "JavaScript ", 
-        "D365 CRM "
+        "MS Dynamics 365"
       ],
+      description: "",
       client: "Euronext",
-      date: "2023-08"
+      date: "2023-08",
+      progression: "New role!"
   },
   {
     company: "n4IT",
@@ -38,27 +34,16 @@ const experiences=[
       "Power Automate ",
       "Power Apps "
     ],
+    description: "",
     client: "Internal",
-    date: "2023-02"
-  },
-  {
-    company: "test 2",
-    role: "test 2",
-    stack: [
-      "Angular.js ",
-      ".Net C# ", 
-      "JavaScript ", 
-      "D365 CRM "
-    ],
-    client: "test 2",
-    date: "test 2"
+    date: "2023-02",
+    progression: ""
   }
 ]
 
 const Page = () => {
-
-  const handleGithubClick = (projectName: string) => {
-    window.open("https://github.com/MarcoFz7/" + projectName);
+  const handleLinkedInBtn = () => {
+    window.open("https://pt.linkedin.com/in/marco-ferraz-03042025a");
   }
 
   return (
@@ -66,13 +51,18 @@ const Page = () => {
       <span className='page-header'>
         Experience
       </span>
+      <div className='page-sub-header'>
+        <button type='button' title='LinkedIn' className='linkedIn-btn' onClick={handleLinkedInBtn}>
+            <FaLinkedinIn/>
+        </button>       
+      </div>
       <div className="experiences-list-timeline"> 
         <ul className="timeline">
           {
           experiences.map((item) => (                       
             <li className="timeline-item" key={item.role + item.company}>
               <div className='timeline-item-content'>
-                <div className="timeline-item-header">
+                <div className="timeline-item-header">              
                   <span className="timeline-item-role">{item.role}</span>
                   <span className="timeline-item-date">{item.date}</span>
                 </div>
@@ -86,7 +76,13 @@ const Page = () => {
                     <span className='timeline-item-stack-tech' key={tech}>{tech}</span>
                   ))}
                 </div>
-              </div>
+                { item.progression != "" ?
+                  <div className={`timeline-progression${item.progression == "New role!" ? ' role' 
+                                                       : item.progression.includes("company") ? ' company' : ''}`}> 
+                    <span className='progression-text'>{item.progression}</span>
+                  </div>  
+                : null}                    
+              </div>       
             </li>
           ))
           }
