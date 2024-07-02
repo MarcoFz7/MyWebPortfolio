7b363f7bc2279a1dffb638c9e1247165124c7b37
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-07-02 22:51:19 +0100

    #comment Playing with some animations to use the background and apply some effects in the future.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,54 @@ import './globals.css'
 import { useState } from 'react'
 import SideNavBar from './components/sidenavbar/sidenavbar'
 import Providers from './providers'
-import { Dancing_Script } from 'next/font/google'
+import { Poppins, Dancing_Script } from 'next/font/google'
 
 const dancingScript = Dancing_Script({weight: '600', style: "normal", subsets: ["latin"]})
+const poppins = Poppins({ weight: ['200', '300', '400', '500', '600'], style: "normal", subsets: ["latin"] });
+
+
+/*
+ * Interface used for the figures used in the background animation
+ * Specifies their color (shuffled everytime from a limited set of colors), start position in the x and y axis and end position in the x axis
+ */
+interface Figure {
+  color: string;
+  startPositionX: string;
+  startPositionY: string;
+}
+
+// Function to shuffle an array - used for colors
+function shuffleArray<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// Array of colors to be used in figures array
+const colors: string[] = [
+  '#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#33FFF5',
+  '#F5FF33', '#FF8C33', '#8C33FF', '#33FF8C', '#FF3333',
+  '#33FF33', '#3333FF', '#FF33FF', '#33FFA1', '#A133FF',
+  '#FFA133', '#FF338C', '#33A1FF', '#A1FF33', '#FF33F5'
+];
+
+// Arrays of positions in X to be used 
+const startPositions: string[] = [
+  "0", "5%", "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", 
+  "50%", "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%"
+];
+
+// Shuffle the colors 
+const shuffledColors = shuffleArray(colors);
+
+// Array of the set of colors able to be used in the figures - right now 20 colors for 20 figures
+let figures: Figure[] = shuffledColors.map((color, index) => ({
+  color: color,
+  startPositionX: startPositions[index],
+  startPositionY: startPositions[index]
+}));
 
 export default function RootLayout({
   children,
@@ -15,6 +60,7 @@ export default function RootLayout({
 }) {
   const [itemName, setItemName] = useState("Home");
   const [sidebarNotification, setSidebarNotification] = useState(false);
+  const [isMoving, setIsMoving] = useState(false);
 
   const handleItemClick = (itemName: string) => {
     setItemName(itemName);
@@ -64,14 +110,35 @@ export default function RootLayout({
       setTimeout(() => {
         setHtmlBlur(false);
         setReady(true);
+
+        setIsMoving(true);
+
+       /*  setTimeout(() => {
+          setIsMoving(false);
+          figures = figures.map((item) => ({
+            ...item,
+            startPositionX: "0"
+          }));
+        }, 2500); */
+
       }, 325);
     }, 325);
   };
 
   return (
     <html lang="en" className='root-html' onClick={sidebarNotification ? undefined : handleHtmlClick} style={{filter: htmlBlur ? 'blur(1.5px)' : 'blur(0px)'}}>
-      <body>
+      <body className={`${poppins.className} nutrition-page`}>
         <Providers onProvidersReady={handleProvidersReadiness}>
+          {/* <div className='screen-background-addorns'>
+            {
+              figures.map((item, index) => (                          
+                <div
+                  key={`cube-${index}`}
+                  className={`cube${isMoving ? ' move' : ''} bg-[${item.color}]`}
+                  style={{ left: `${item.startPositionX}`, top: `${item.startPositionY}`}}
+                ></div>))
+            }   
+          </div> */}
           <div className='main-screen'>
             <div id="main-screen-addorns" className="main-screen-page">
               <div id="main-content">
